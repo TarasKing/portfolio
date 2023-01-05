209b57a1de0713c7922f61f6cdb6d7ad618c94b1
TarasKing <ivanovtaras@live.ru>
2023-01-04 20:00:22 -0800

    add files #1

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Hero from "../components/Hero"
+import Services from "../components/Services"
+import Jobs from "../components/Jobs"
+import Projects from "../components/Projects"
+import Seo from "../components/Seo"
+import Layout from "../components/Layout"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Index page</h1>
+    </Layout>
+  )
+}
+
+export default IndexPage
