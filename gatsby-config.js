8f2e1d1be492f7c922b8a4cb68f48116224e2506
TarasKing <ivanovtaras@live.ru>
2023-04-02 20:42:37 -0700

    added google analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,15 +8,6 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-// require("dotenv").config()
-
-// const strapiConfig = {
-//   apiURL: process.env.STRAPI_API_URL,
-//   accessToken: process.env.STRAPI_TOKEN,
-//   collectionTypes: ["job", "project", "blog"],
-//   singleTypes: ["about"],
-// }
-
 module.exports = {
   siteMetadata: {
     title: "Taras Ivanov's Portfolio",
@@ -65,12 +56,26 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    // {
-    //   resolve: `gatsby-transformer-cloudinary`,
-    //   options: {
-    //     // Add the `gatsbyImageData` resolver to `CloudinaryMedia`
-    //     transformTypes: [`CloudinaryMedia`],
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GA_TRACKING_ID, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body / was false
+          head: true,
+        },
+      },
+    },
   ],
 }
